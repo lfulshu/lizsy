ffd2631957bd3a7eae2ac283e780ae84ee5e5ea7
liizzz <liizzz@foxmail.com>
2019-09-27 09:58:40 +0800

    MD5

---
+++ src/main/java/com/yx/util/MD5Util.java
@@ -0,0 +1,18 @@
+package com.yx.util;
+
+/**
+ * @author liizzz
+ * @start 2019-09-26 17:05
+ */
+public class MD5Util {
+
+    private static String byteArrayToHexString(byte[] b){
+        String str = new String(b);
+        return str;
+    }
+
+    public static String byteToHexString(byte b){
+
+        return null;
+    }
+}
